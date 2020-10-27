8a45b49b6fb6b83aef19f267b8f9b0b86d89feea
Kevin Browne <kbrowne97@gmail.com>
2020-10-26 20:29:33 -0400

    Used event delegation to listen for mouseover on grid-container children

--- app.js
+++ app.js
@@ -2,14 +2,67 @@ const gridContainer = document.querySelector("#grid-container");
 const square = document.createElement("div");
 const slider = document.querySelector("#grid-slider");
 const displaySlider = document.querySelector("#sliderValue")
+const button = document.querySelector("ul");
 gridContainer.appendChild(square);
-const gridSquare = square.classList.add("squareClass");
-square.style.backgroundColor = "red";
+square.style.backgroundColor = "white";
+
+let color = "black";
+
+//Get color selection from buttons and if "clear" is chosen
+//reset the grid
+
+button.addEventListener('click', function (e) {
+    if (e.target.id === "white") {
+        slider.onchange();
+    }
+    else color = e.target.id;
+});
+
+//Random color generator ty stackoverflow
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+rgbValue = [];
+
+function makeMoreGray(){
+    rgbValue[0] += 25;
+    rgbValue[1] += 25;
+    rgbValue[2] += 25;
+    return rgbValue;
+} 
+
+/////////////////////////////////////////////////////////////////
+//convert color selection into an expression that can be passed//
+//to colorChange()  Maybe add grayscale later                  //
+/////////////////////////////////////////////////////////////////
+function setColor() {
+    if (color === "grayscale"){
+        return makeMoreGray();
+    }else if (color === "rave"){
+        return getRandomColor();
+    }else if(color === "black"){
+        return "black";
+    }
+}
+
+function colorChange(event) {
+    rgbValue = event.target.style.backgroundColor;
+    event.target.style.backgroundColor = setColor();
+}
+
+gridContainer.addEventListener("mouseover", colorChange, false);
 
 slider.onchange = function() {
     displaySlider.textContent = slider.value;  
     let gridRow = [];
     let gridColumn = [];
+    square.style.backgroundColor = "white";
     while (gridContainer.hasChildNodes()) {
         gridContainer.removeChild(gridContainer.lastChild);
     }
@@ -17,19 +70,13 @@ slider.onchange = function() {
         gridColumn.push("auto");
         gridRow.push("auto");
     }
+   
     gridContainer.style.gridTemplateColumns = gridColumn.join(" ");
     gridContainer.style.gridTemplateRows = gridRow.join(" ");
-    console.log(gridRow);
-    console.log(gridRow.join(" "));
+   
     for (let i=0; i < Math.pow(slider.value, 2); i++) {
         gridContainer.appendChild(square.cloneNode(true));
-    }
-    const allSquares = document.querySelectorAll(".squareClass");
-    allSquares.forEach((squareClass) =>{
-        squareClass.addEventListener("mouseover", () => {
-            console.log("test");
-        });
-    });
+    }   
 }
 
 
